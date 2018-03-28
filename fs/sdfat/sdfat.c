8385c4a563274ad6f9186913f469139d1bd1e9f1
Paul Keith <javelinanddart@gmail.com>
2018-03-28 19:52:29 +0200

    fs: sdfat: add MODULE_ALIAS_FS for supported filesystems
    Change-Id: I2a47501eb66c14629f0284d048df4116dcef25a1

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -5228,34 +5228,6 @@ static struct file_system_type vfat_fs_type = {
 MODULE_ALIAS_FS("vfat");
 #endif /* CONFIG_SDFAT_USE_FOR_VFAT */
 
-#ifdef CONFIG_SDFAT_USE_FOR_EXFAT
-static struct file_system_type exfat_fs_type = {
-	.owner       = THIS_MODULE,
-	.name        = "exfat",
-	.mount       = sdfat_fs_mount,
-#ifdef CONFIG_SDFAT_DBG_IOCTL
-	.kill_sb    = sdfat_debug_kill_sb,
-#else
-	.kill_sb    = kill_block_super,
-#endif /* CONFIG_SDFAT_DBG_IOCTL */
-	.fs_flags    = FS_REQUIRES_DEV,
-};
-#endif /* CONFIG_SDFAT_USE_FOR_EXFAT */
-
-#ifdef CONFIG_SDFAT_USE_FOR_VFAT
-static struct file_system_type vfat_fs_type = {
-	.owner       = THIS_MODULE,
-	.name        = "vfat",
-	.mount       = sdfat_fs_mount,
-#ifdef CONFIG_SDFAT_DBG_IOCTL
-	.kill_sb    = sdfat_debug_kill_sb,
-#else
-	.kill_sb    = kill_block_super,
-#endif /* CONFIG_SDFAT_DBG_IOCTL */
-	.fs_flags    = FS_REQUIRES_DEV,
-};
-#endif /* CONFIG_SDFAT_USE_FOR_VFAT */
-
 static int __init init_sdfat_fs(void)
 {
 	int err;
